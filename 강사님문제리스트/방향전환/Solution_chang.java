b37b84ea11c4f6ec13a6ceadf94dec2feef8b1ee
changhyeon12333 <ckd5508@naver.com>
2024-01-20 23:59:07 +0900

    방향전환

--- 강사님문제리스트/방향전환/Solution_chang.java
+++ 강사님문제리스트/방향전환/Solution_chang.java
@@ -1,10 +1,38 @@
-package edu.ssafy.chap01;
 
-public class CallByTest {
+import java.util.Scanner;
 
+public class Solution {
+
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		for (int tc =0 ; tc < T ; tc ++) {
+			int[] start = new int[2];
+			int[] end = new int[2];
+			int x_diff = Integer.MIN_VALUE;
+			int y_diff = Integer.MIN_VALUE;
+			int ans = 0;
+			
+			start[0] = sc.nextInt();
+			start[1] = sc.nextInt();
+			end[0] = sc.nextInt();
+			end[1] = sc.nextInt();
+			x_diff = Math.max(Math.abs(start[0] - end[0]),x_diff);
+			y_diff = Math.max(Math.abs(start[1] - end[1]), y_diff);
+			
+			ans =Math.max(x_diff,y_diff) * 2;
+			
+			if ((x_diff+y_diff) %2 ==1) {
+				ans = ans -1;
+			}
+			
+			System.out.printf("#%d %d\n", tc+1, ans);
+		}
+	
 
 	}
-
+	
 }

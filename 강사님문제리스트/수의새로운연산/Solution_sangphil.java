518f3ac17a7af36bbd10a844d5a5302620f1bbbb
박상필 <pagerank@bagsangpil-ui-MacBookPro.local>
2024-01-22 00:23:13 +0900

    docs: add directory

---
+++ 강사님문제리스트/수의새로운연산/Solution_sangphil.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution_sangphil {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("input.txt"));
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for (int t = 0; t < T; t++) {
+
+        }
+        sc.close();
+    }
+}

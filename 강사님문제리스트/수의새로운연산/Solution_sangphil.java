3a66a8f76b1a752bb817e0d1cb6cad5a8aad7bdb
sppark112 <sppark112@naver.com>
2024-01-22 15:02:04 +0900

    feat: solve program solving problem in SWEA

--- 강사님문제리스트/수의새로운연산/Solution_sangphil.java
+++ 강사님문제리스트/수의새로운연산/Solution_sangphil.java
@@ -2,15 +2,53 @@ import java.util.*;
 import java.io.*;
 
 public class Solution_sangphil {
-    public static void main(String[] args) throws IOException {
-        System.setIn(new FileInputStream("input.txt"));
-        Scanner sc = new Scanner(System.in);
+	static int T, p, q, ans;
+	
+	public static void main(String[] args) throws IOException {
+		//System.setIn(Solution_sangphil.class.getResourceAsStream("input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		T = Integer.parseInt(br.readLine());
+		
+		for (int t = 0; t < T; t++) {
+			st = new StringTokenizer(br.readLine());
+			p = Integer.parseInt(st.nextToken());
+			q = Integer.parseInt(st.nextToken());
+			ans = 0;
+			
+			
+			int total = 1;	// 가장 근접한 1열에 놓일 값
+			int weight = 0; // 가장 근접한 행 값 
+			while (total + weight <= p) {
+				total += weight;
+				weight += 1;
+			}
+			int diff = p - total;
+			int y = weight - diff;
+			int x = 1 + diff;
 
-        int T = sc.nextInt();
+			
+			total = 1;
+			weight = 0;
+			while (total + weight <= q) {
+				total += weight;
+				weight += 1;
+			}
+			diff = q - total;
+			y += weight - diff;
+			x += 1 + diff;
 
-        for (int t = 0; t < T; t++) {
-
-        }
-        sc.close();
-    }
+			
+			int totalY = x + y - 1; // 가장 근접한 y 값
+			
+			for (int i = 1; i < totalY; i++) {
+				ans += i; 
+			}
+			ans ++; // 가장 근접한 y 좌표를 갖는 실제 값
+			ans += totalY - y;
+			
+			System.out.printf("#%d %d\n", t+1, ans);
+		}
+	}
 }

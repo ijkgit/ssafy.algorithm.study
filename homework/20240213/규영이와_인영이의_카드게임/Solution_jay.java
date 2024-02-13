14b85e2807bbc4af7206b70dce7f78627b21250c
Jay Im <83939775+ijkgit@users.noreply.github.com>
2024-02-13 14:39:43 +0900

    ✨feat: BOJ_6808_규영이와_인영이의_카드게임 알고리즘 구현

---
+++ homework/20240213/규영이와_인영이의_카드게임/Solution_jay.java
@@ -0,0 +1,87 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+	private final static int SIZE = 9;
+	private int[] arr;
+	private int[] card;
+	private int[] sel;
+	private int sw;
+	private int aw;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		new Solution().io();
+	}
+
+	private void io() throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		int testCase = Integer.parseInt(br.readLine());
+
+		for (int t = 1; t <= testCase; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			arr = new int[SIZE];
+			for (int i = 0; i < SIZE; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			card = new int[SIZE];
+			int idx = 0;
+			for (int i = 1; i <= SIZE * 2; i++) {
+				boolean flag = true;
+				for (int j = 0; j < SIZE; j++) {
+					if (arr[j] == i) {
+						flag = false;
+						break;
+					}
+				}
+				if(flag) {
+					card[idx] = i;
+					idx++;
+				}
+			}
+			sw = 0; aw = 0;
+			sol();
+			sb.append("#").append(t).append(" ").append(aw).append(" ").append(sw).append("\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+
+	private void sol() {
+		sel = new int[SIZE];
+		permutation(0, 0);
+	}
+
+	private void permutation(int k, int v) {
+		if (k == SIZE) {
+			int ss = 0, as = 0;
+			for (int i = 0; i < SIZE; i++) {
+				if(sel[i] > arr[i]) {
+					ss += sel[i] + arr[i];
+				} else {
+					as += sel[i] + arr[i];
+				}
+			}
+			if (ss > as) {
+				sw += 1;
+			} else if (as > ss) {
+				aw += 1;
+			}
+			return;
+		}
+
+		for (int i = 0; i < SIZE; i++) {
+			if ((v & (1 << i)) == 0) {
+				sel[k] = card[i];
+				permutation(k + 1, v | 1 << i);
+			}
+		}
+	}
+}

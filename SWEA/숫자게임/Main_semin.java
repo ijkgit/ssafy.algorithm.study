be25ca144ae2d16a89d1f10259c315174fa3125d
Semin Kim <65807803+seminss@users.noreply.github.com>
2024-02-18 11:36:09 +0900

    ✨feat: SWEA_7206_숫자_게임_시초_해결!!

--- SWEA/숫자게임/Main_semin.java
+++ SWEA/숫자게임/Main_semin.java
@@ -1,84 +1,86 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
-/**시초 떠요... 😊 
- * 정답을 봤는데, 지금의 저에게는 불가능한 풀이입니다. ㅎㅎㅎ */
-public class Main_semin {
+/** 시간초과 해결!! */
+public class Solution_semin {
+    static int size, Ans;
+    static Set<Integer> newNums;
+    static int[] dp = new int[99999];
 
-	static int size, Ans;
-	static Set<Integer> newNums;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            sb.append("#").append(tc).append(" ");
+            String number = br.readLine();
+            Ans = 0;
+            Arrays.fill(dp, -1);
+            recursive(number, 0);
+            sb.append(Ans).append("\n");
+        }
+        System.out.println(sb);
+    }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+    private static void recursive(String number, int depth) {
+        int num = Integer.parseInt(number);
+        if (dp[num] != -1) {
+            Ans = Math.max(dp[num] + depth, Ans);
+            return;
+        }
+        if (number.length() <= 1) {
+            Ans = Math.max(depth, Ans);
+            dp[num] = 0;
+            return;
+        }
+        newNums = new HashSet<>();
+        for (int i = 2; i <= number.length(); i++) {
+            size = i; // 1~n-1
+            int[] idxs = new int[size];
+            idxs[size - 1] = number.length() - 1; // 맨 끝 인덱스 넣기
+            idxCombination(0, 0, idxs, number);
+        }
+        int maxTurn = 0; //현재 숫자로 시작해서 1자리 수가 될 때까지의 최대 턴 수
+        for (int newNum : newNums) {
+            recursive(String.valueOf(newNum), depth + 1);
+            maxTurn = Math.max(maxTurn, dp[newNum]);
+        }
+        dp[num] = maxTurn + 1;
+        Ans = Math.max(depth + dp[num], Ans);
+    }
 
-		int T = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc <= T; tc++) {
-			sb.append("#").append(tc).append(" ");
-			int number = Integer.parseInt(br.readLine());
-			Ans = 0;
-			recursive(number, 0);
-			sb.append(Ans).append("\n");
-		}
-		System.out.println(sb);
-	}
+    private static void idxCombination(int idx, int k, int[] idxs, String number) {
+        if (k == size - 1) { // 인덱스의 조합
+            int mul = calculateMul(idxs, number);
+            newNums.add(mul);
+            return;
+        }
+        if (idx == number.length() - 1) { // 쪼개는 개수는 숫자 -1
+            return;
+        }
+        idxs[k] = idx;
+        idxCombination(idx + 1, k + 1, idxs, number);
+        idxCombination(idx + 1, k, idxs, number);
+    }
 
-	private static void recursive(int number, int depth) {
-		List<Character> nums = new ArrayList<>();
-		for (Character ch : Integer.toString(number).toCharArray()) {
-			nums.add(ch);
-		}
-		if (nums.size() <= 1) {
-			Ans = Math.max(depth, Ans);
-			return;
-		}
-
-		newNums = new HashSet<>();
-		for (int i = 2; i <= nums.size(); i++) {
-			size = i; // 1~n-1
-			int[] idxs = new int[size];
-			idxs[size - 1] = nums.size() - 1; // 맨 끝 인덱스 넣기
-			idxCombination(0, 0, idxs, nums);
-		}
-		for (int num : newNums) {
-			recursive(num, depth + 1);
-		}
-	}
-
-	private static void idxCombination(int idx, int k, int[] idxs, List<Character> nums) {
-		if (k == size - 1) { // 인덱스의 조합
-			int mul = calculateMul(idxs, nums);
-			newNums.add(mul);
-			return;
-		}
-		if (idx == nums.size() - 1) { // 쪼개는 개수는 숫자 -1
-			return;
-		}
-		idxs[k] = idx;
-		idxCombination(idx + 1, k + 1, idxs, nums);
-		idxCombination(idx + 1, k, idxs, nums);
-	}
-
-	private static int calculateMul(int[] idxs, List<Character> nums) {
-		int mul = 1;
-		String temp = "";
-		int cnt = 0; // split 할 기준
-		for (int i = 0; i < nums.size(); i++) {
-			if (i < idxs[cnt]) {
-				temp += nums.get(i);
-			}
-			if (i == idxs[cnt]) {
-				temp += nums.get(i);
-				mul *= Integer.valueOf(temp);
-				temp = "";
-				cnt++; // 기준을 업데이트 한다.
-			}
-		}
-		return mul;
-	}
+    private static int calculateMul(int[] idxs, String number) {
+        char[] nums = number.toCharArray();
+        int mul = 1;
+        String temp = "";
+        int cnt = 0; // split 할 기준
+        for (int i = 0; i < nums.length; i++) {
+            if (i < idxs[cnt]) {
+                temp += nums[i];
+            }
+            if (i == idxs[cnt]) {
+                temp += nums[i];
+                mul *= Integer.valueOf(temp);
+                temp = "";
+                cnt++; // 기준을 업데이트 한다.
+            }
+        }
+        return mul;
+    }
 }

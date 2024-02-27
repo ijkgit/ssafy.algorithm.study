385b1a53e75eb39db05ed63cae3b53cb74fc3d2a
Sangphil Park <81211140+SangphilPark@users.noreply.github.com>
2024-02-27 17:36:12 +0900

    ✨feat: SWEA_7206_숫자_게임 실패

--- SWEA/숫자_게임/Solution_sangphil.java
+++ SWEA/숫자_게임/Solution_sangphil.java
@@ -1,67 +1,54 @@
 import java.util.*;
 import java.io.*;
 
-public class Solution {
-	static int ans, n;
+/*
+ * 		[SWEA 7206] 숫자 게임
+ * 			- DP
+ */
+public class Solution_sangphil {
+	static int ans;
+	static String n;
+	
 	
 	public static void main(String[] args) throws IOException {
 		System.setIn(Main.class.getResourceAsStream("input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st = null;
 		StringBuilder sb = new StringBuilder();
 		
+		int T = Integer.parseInt(br.readLine());
+		
 		for (int t = 1; t <= T; t++) {
-			ans = Integer.MIN_VALUE;
+			ans = 0;
+			n = br.readLine();
 			
-			n = Integer.parseInt(br.readLine());
-			int size = (int)(Math.log10(n)+1);
-			int left = n / (int)Math.pow(10, size-1);
-			int right = n % (int)Math.pow(10, size-1);
-			divide(1, left, right, 1);
+			divide(11, "", n, 0);
 			
-			System.out.println(ans);
 			sb.append(String.format("#%d %d\n", t, ans));
 		}
 		System.out.println(sb);
 	}
 	
-	// 곱하기 확정된 수 // 왼쪽수 // 오른쪽 수
-	// 1. 곱하면서 왼쪽 초기화
-	// 2. 안곱하고, 왼쪽 <-> 오른쪽
-	static void divide (int num, int left, int right, int depth) {
-		System.out.println(num + " " + left + " " + right + " " + depth);
-		if (num < 10 && left == 0 && right == 0) {
-			ans = Math.max(ans, depth);
+	static void divide(int N, String left, String right, int cnt) {
+		if (N < 10 && right.length() == 0) {
+			ans = Math.max(ans, cnt);
 			return;
 		}
 		
-
-		if (left == 0 && right == 0) {
-			int lengthN = (int)(Math.log10(num)+1);
-			divide(1, num / (int)Math.pow(10, lengthN-1), num % (int)Math.pow(10, lengthN-1), depth+1);
-			return;
+		if (!left.equals("")) {
+			int l = Integer.parseInt(left);
+			int r = Integer.parseInt(right);
+			divide(l*r, "", "", cnt + 1);
+			String ll = "";
+			String rr = ""+(l*r);
+			if (rr.length() > 1) {
+				divide(l*r, ll + rr.substring(0,1), rr.substring(1, rr.length()), cnt + 1);				
+			}
 		}
-		
-		if (left == 0) {
-			divide (num*right, 0, 0, depth);
-			return;
+		if (right.length() > 1) {
+//			System.out.println(left + right.substring(0,1));
+//			System.out.println(right.substring(1,right.length()));
+			divide(N, left + right.substring(0,1), right.substring(1,right.length()), cnt);
 		}
-		if (right == 0) {
- 			divide(num*left, 0, 0, depth);
-			return;
-		}
-
-		int lengthR = (int)(Math.log10(right)+1);
-		while (lengthR >= 1) {
-			divide(num*left*right, 0, 0, depth);
-			
-			divide(num * left, right / (int)Math.pow(10, lengthR-1), right % (int)Math.pow(10, lengthR-1), depth);
-			
-			left = left*10 + right / (int)Math.pow(10, lengthR-1);
-			right %= (int)Math.pow(10, lengthR-1);
-			
-			lengthR = (int)(Math.log10(right)+1);
-		}
-		
 	}
 }

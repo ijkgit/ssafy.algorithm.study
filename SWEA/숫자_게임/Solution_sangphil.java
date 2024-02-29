0d3a3887c27be64ef240c5a93dcf19740f77ce3a
Sangphil Park <81211140+SangphilPark@users.noreply.github.com>
2024-02-29 20:46:45 +0900

    숫자게임 도전 중

--- SWEA/숫자_게임/Solution_sangphil.java
+++ SWEA/숫자_게임/Solution_sangphil.java
@@ -5,24 +5,23 @@ import java.io.*;
  * 		[SWEA 7206] 숫자 게임
  * 			- DP
  */
-public class Solution_sangphil {
+public class Solution {
+	static int[] memo = new int[99999+1];
 	static int ans;
-	static String n;
-	
+	static int n;
 	
 	public static void main(String[] args) throws IOException {
-		System.setIn(Main.class.getResourceAsStream("input.txt"));
+		// System.setIn(Main.class.getResourceAsStream("input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
 		StringBuilder sb = new StringBuilder();
 		
 		int T = Integer.parseInt(br.readLine());
-		
 		for (int t = 1; t <= T; t++) {
 			ans = 0;
-			n = br.readLine();
-			
-			divide(11, "", n, 0);
+			n = Integer.parseInt(br.readLine());
+            
+			divide(n, "", "", 0);
 			
 			sb.append(String.format("#%d %d\n", t, ans));
 		}
@@ -30,25 +29,40 @@ public class Solution_sangphil {
 	}
 	
 	static void divide(int N, String left, String right, int cnt) {
-		if (N < 10 && right.length() == 0) {
-			ans = Math.max(ans, cnt);
+		if (right.length() == 0) {
+			if (N < 10) {
+				ans = Math.max(ans, cnt);
+				if (memo[N] < ans) memo[N] = ans;
+			} else {
+				if (memo[N] == 0) {
+					String str = ""+N;
+					String ll = left + str.substring(0, 1);
+					String rr = str.substring(1, str.length());
+					divide(1, ll, rr, cnt+1);					
+				} else {
+					divide(0, "", "", memo[N]);
+				}
+			}
 			return;
 		}
 		
 		if (!left.equals("")) {
 			int l = Integer.parseInt(left);
 			int r = Integer.parseInt(right);
-			divide(l*r, "", "", cnt + 1);
-			String ll = "";
-			String rr = ""+(l*r);
-			if (rr.length() > 1) {
-				divide(l*r, ll + rr.substring(0,1), rr.substring(1, rr.length()), cnt + 1);				
-			}
+			divide(N*l*r, "", "", cnt);	// 넘기기 전 전부 곱하기
+			divide(N*l, "", right, cnt);	// 넘기기 전 일부 곱하기
 		}
 		if (right.length() > 1) {
-//			System.out.println(left + right.substring(0,1));
-//			System.out.println(right.substring(1,right.length()));
+			// 넘기기
 			divide(N, left + right.substring(0,1), right.substring(1,right.length()), cnt);
+		} else {
+			// 넘길 수 없다면?
+			if (left.equals("")) divide(N*casting(right), "", "", cnt);
 		}
+	}
+	
+	static int casting(String str) {
+		if (str.length() == 0) return 1;
+		return Integer.parseInt(str);
 	}
 }

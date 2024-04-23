7a06fa269e443bf79d5f5af2ab6d9fdda1968b93
Jay Im <83939775+ijkgit@users.noreply.github.com>
2024-04-23 17:39:01 +0900

    ✏️comment: CODETREE_토스트 계란틀 주석 추가

--- CODETREE/토스트_계란틀/Main_jay.java
+++ CODETREE/토스트_계란틀/Main_jay.java
@@ -33,13 +33,40 @@ public class Main {
 
     private static int sol() {
         int ans = 0;
-        while (makeUnion() != 0) {
+        while (makeUnion() != 0) { // 분리된 계란 틀의 수가 0이라면, 계란의 이동이 더 이상 필요 없는 경우
             move();
-            ans++;
+            ans++; // 이동 횟수 세주기
         }
         return ans;
     }
 
+    private static int makeUnion() {
+        int cnt = 0;
+
+        // 선의 분리를 통해 합쳐진 계란틀의 계란은 하나로 합치고 이후에 다시 분리한다.
+        initUnion(); // 계란틀 분리하기
+
+        // 모든 계란 틀에 대해 검사를 실시하여 계란틀의 선을 분리
+        for (int x = 0; x < N; x++) {
+            for (int y = 0; y < N; y++) {
+                for (int[] d : direction) {
+                    int nx = x + d[0];
+                    int ny = y + d[1];
+
+                    if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+                    if (!check(map[x][y], map[nx][ny])) continue;
+
+                    // 두 계란틀의 계란의 양의 차이가 L 이상 R 이하라면 계란틀의 해당 선을 분리
+                    setUnion(union[nx][ny], union[x][y]);
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt; // 분리된 계란 틀의 수를 반환
+    }
+
+    // 계란틀을 만들어준다.
     private static void initUnion() {
         union = new int[N][N];
         int num = 1;
@@ -50,25 +77,9 @@ public class Main {
         }
     }
 
-    private static int makeUnion() {
-        int cnt = 0;
-
-        initUnion();
-
-        for (int x = 0; x < N; x++) {
-            for (int y = 0; y < N; y++) {
-                for (int[] d : direction) {
-                    int nx = x + d[0];
-                    int ny = y + d[1];
-
-                    if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-                    if (!check(map[x][y], map[nx][ny])) continue;
-                    setUnion(union[nx][ny], union[x][y]);
-                    cnt++;
-                }
-            }
-        }
-        return cnt;
+    // 계란틀의 해당 선을 분리할 수 있는지 확인
+    private static boolean check(int a, int b) {
+        return L <= Math.abs(a - b) && Math.abs(a - b) <= R;
     }
 
     private static void setUnion(int u, int n) {
@@ -79,8 +90,9 @@ public class Main {
         }
     }
 
+    // 각 계란이 변동된 내용을 갱신해주기
     private static void move() {
-        visited = new boolean[N][N];
+        visited = new boolean[N][N]; // 방문 배열은 공용으로 사용
         for (int x = 0; x < N; x++) {
             for (int y = 0; y < N; y++) {
                 bfs(x, y);
@@ -89,8 +101,8 @@ public class Main {
     }
 
     private static void bfs(int x, int y) {
-        ArrayDeque<int[]> queue = new ArrayDeque<>();
-        ArrayDeque<int[]> unions = new ArrayDeque<>();
+        ArrayDeque<int[]> queue = new ArrayDeque<>(); // 탐색을 위한 큐
+        ArrayDeque<int[]> unions = new ArrayDeque<>(); // 연합끼리 모으기 위한 큐
         visited[x][y] = true;
         queue.offer(new int[]{x, y});
         unions.offer(new int[]{x, y});
@@ -106,16 +118,16 @@ public class Main {
                 if (visited[nx][ny]) continue;
                 if (union[x][y] != union[nx][ny]) continue;
 
+                // 합쳐질 계란들의 총 합을 구해주고, 각 큐에 좌표를 넣어준다.
                 visited[nx][ny] = true;
                 sum += map[nx][ny];
                 queue.offer(new int[]{nx, ny});
                 unions.offer(new int[]{nx, ny});
             }
         }
-        for (int[] u : unions) map[u[0]][u[1]] = sum / unions.size();
-    }
 
-    private static boolean check(int a, int b) {
-        return L <= Math.abs(a - b) && Math.abs(a - b) <= R;
+        // 각 계란틀별 계란의 양은 (합쳐진 계란의 총 합)/(합쳐진 계란틀의 총 개수)
+        // 연합 계란들의 값을 갱신해준다.
+        for (int[] u : unions) map[u[0]][u[1]] = sum / unions.size();
     }
 }

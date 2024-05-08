70ca9eb3e6621f6240ac68125f1f9383b3921642
Jiwan Seo <91694379+wannaseoji@users.noreply.github.com>
2024-05-08 22:26:09 +0900

    ✨feat: BOJ_2531_회전 초밥 알고리즘 구현

--- BOJ/회전_초밥/Main_Jiwan.java
+++ BOJ/회전_초밥/Main_Jiwan.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class S1_2531 {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
